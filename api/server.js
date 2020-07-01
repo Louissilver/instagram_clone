7383450598f742c9f3321dccbcd210655d4efc97
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-06-30 22:10:23 -0300

    Inserção e remoção de comentários e finalização do projeto

--- api/server.js
+++ api/server.js
@@ -12,6 +12,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(multiparty());
 
+app.use(function(req, res, next){
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+
+});
+
 var port = 8080;
 
 app.listen(port);
@@ -31,20 +42,17 @@ app.get('/', function(req, res){
 // POST(create)
 app.post('/api', function(req, res){
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
     var date = new Date();
-    time_stamp = date.getTime();
+    var time_stamp = date.getTime();
 
     var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
-
     var path_origem = req.files.arquivo.path;
     var path_destino = './uploads/' + url_imagem;
 
     fs.rename(path_origem, path_destino, function(err){
         if(err){
-            res.status(500).json({ error: err});
-            return
+            res.status(500).json({ error : err });
+            return;
         }
 
         var dados = {
@@ -64,16 +72,13 @@ app.post('/api', function(req, res){
                 });
             });
         });
-    });
 
-    
+    });
 });
 
 // GET(ready)
 app.get('/api', function(req, res){
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
+    
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.find().toArray(function(err, results){
@@ -90,9 +95,6 @@ app.get('/api', function(req, res){
 
 // GET by ID(ready)
 app.get('/api/:id', function(req, res){
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.find(objectId(req.params.id)).toArray(function(err, results){
@@ -108,30 +110,34 @@ app.get('/api/:id', function(req, res){
 });
 
 app.get('/imagens/:imagem', function(req, res){
-
     var img = req.params.imagem;
 
-    fs.readFile('./uploads/'+ img, function(err, content){
+    fs.readFile('./uploads/' + img, function(err, content){
         if(err){
-            res.status(400).json(err);
+            res.status(400).json({ err });
             return;
         }
 
-        res.writeHead(200, { 'content-type' : 'image/jpg', 'content-type' : 'image/png', 'content-type' : 'image/jpeg' });
+        res.writeHead(200, { 'content-type' : 'image/jpg' });
         res.end(content);
-    })
+
+    });
 });
 
 // PUT by ID(update)
 app.put('/api/:id', function(req, res){
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
+    
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.update(
                 { _id : objectId(req.params.id) },
-                { $set : { titulo : req.body.titulo }},
+                { $push : {
+                        comentarios : {
+                            id_comentario : new objectId(),
+                            comentario : req.body.comentario
+                        }
+                    }
+                },
                 {},
                 function(err, records){
                     if(err){
@@ -144,17 +150,21 @@ app.put('/api/:id', function(req, res){
             );
         });
     });
+    
 });
 
 // DELETE by ID(delete)
 app.delete('/api/:id', function(req, res){
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
-            collection.remove(
-                { _id : objectId(req.params.id) },
+            collection.update(
+                { },
+                { $pull: {
+                            comentarios : { id_comentario : objectId(req.params.id)}
+                         }
+                },
+                { multi : true},
                 function(err, records){
                     if(err){
                         res.json(err);
